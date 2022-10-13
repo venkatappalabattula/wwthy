c9f89b32167a940e900cff60027107a77b3f5acf
Venkat <venkat190402@gmail.com>
2022-10-13 14:13:56 +0530

    Api data retrieval and baseline GUI

--- base/movie.py
+++ base/movie.py
@@ -2,7 +2,7 @@ import requests
 #
 genres = {}
 reverse_genres = {}
-allin = requests.get("https://api.themoviedb.org/3/genre/movie/list?api_key=34fe3fc711aaf02629366ba6335190d4&language=en-US").json()['genres']
+allin = requests.get("https://api.themoviedb.org/3/genre/movie/list?api_key= ~&language=en-US").json()['genres']
 genres['none'] = -1
 for x in allin:
     genres[x['name']] = (x['id'], x['name'])
